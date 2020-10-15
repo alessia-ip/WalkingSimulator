07dc1bfe36220f3f941b1b186ec5bc603262146a
Alessia <alessia@mipsscan.com>
2020-10-14 21:16:58 -0400

    Player Prefab Added
    Player has a prefab, but does not have the trigger added just yet! To be added soon!

---
+++ Assets/Script/scr_script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_script : MonoBehaviour
+{
+
+    public GameObject[] crow;
+    public int numberthing;
+    public Outline outlineScr;
+
+    bool isPlayed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(isPlayed == false)
+        {
+            isPlayed = true;
+
+        }
+    }
+
+
+}
