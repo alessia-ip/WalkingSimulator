7dc5a0a8201c949664b3dcc5df383794329df921
Alessia <alessia@mipsscan.com>
2020-10-17 12:46:45 -0400

    Pigeon House
    Added the pigeon's house, and added a script to turn the interior on and off, since the model is see through on the inside. Other interior items can be placed in there. Re-structured the hierarchy of the main scene as well.
    
    Silohettes still need to be hidden through the walls in this scene

---
+++ Assets/Script/scr_disable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_disable : MonoBehaviour
+{
+    public GameObject[] toBeTurnedOff;
+    public GameObject[] toBeTurnedOn;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            for(int i = 0; i < toBeTurnedOff.Length; i++)
+            {
+                toBeTurnedOff[i].SetActive(false);
+            }
+            for (int i = 0; i < toBeTurnedOn.Length; i++)
+            {
+                toBeTurnedOn[i].SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Script/scr_teleporter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_teleporter : MonoBehaviour
+{
+    //the place the player is teleporting to 
+    public GameObject teleportENDPOINT;
+
+    //the player character
+    public GameObject playerObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if the player enters the trigger, teleport them to that trigger's assigned endpoint
+        if(other.gameObject == playerObject)
+        {
+            playerObject.GetComponent<CharacterController>().enabled = false;
+            playerObject.transform.position = teleportENDPOINT.transform.position;
+            playerObject.GetComponent<CharacterController>().enabled = false;
+        }
+    }
+}
