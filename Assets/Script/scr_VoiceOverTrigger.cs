25e471ad888481832e22bdd7286819a6768e49a7
Alessia <alessia@mipsscan.com>
2020-10-16 20:39:30 -0400

    Mirror Prefab Added
    Mirror Prefab added to the prefabs folder
    New scenes folder added for final scenes to go into
    Making trigger Script

---
+++ Assets/Script/scr_VoiceOverTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class scr_VoiceOverTrigger : MonoBehaviour
+{
+    public string dialogueForClip; 
+    public AudioClip voiceoverLine;
+
+    private void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            
+        }
+    }
+
+}
