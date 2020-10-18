003bdeaa0488e11e73bf4a7f7dbe25f1f7c1f72d
Alessia <alessia@mipsscan.com>
2020-10-18 11:15:11 -0400

    Camera Overlay
    Added a camera overlay and screenshot ability works with it

---
+++ Assets/Script/scr_camOverlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_camOverlay : MonoBehaviour
+{
+
+    public GameObject camImage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            camImage.SetActive(true);
+        } else
+        {
+            camImage.SetActive(false);
+        }
+    }
+}
