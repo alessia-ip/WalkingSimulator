0832d692cd96f5a636d9f9b394289a0f15c9e41a
Alessia <alessia@mipsscan.com>
2020-10-23 12:48:31 -0400

    Chromatic Abberation on Zoom
    Adds a chromatic aberation to the zoom camera

--- Assets/Script/scr_camOverlay.cs
+++ Assets/Script/scr_camOverlay.cs
@@ -1,21 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class scr_camOverlay : MonoBehaviour
 {
 
     public GameObject camImage;
+    public PostProcessVolume volume;
 
+
+    private void Start()
+    {
+       
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(1))
         {
             camImage.SetActive(true);
+            ChromaticAberration chromaticAberration;
+            if (volume.profile.TryGetSettings(out chromaticAberration))
+            {
+                chromaticAberration.intensity.value = 1;
+            }
+
         } else
         {
             camImage.SetActive(false);
+            ChromaticAberration chromaticAberration;
+            if (volume.profile.TryGetSettings(out chromaticAberration))
+            {
+                chromaticAberration.intensity.value = 0;
+            }
         }
     }
 }
