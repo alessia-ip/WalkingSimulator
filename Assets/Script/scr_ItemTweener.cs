dfb798237d4baa47c48562ad78be8f2ecfb0c25c
Alessia <alessia@mipsscan.com>
2020-10-24 11:53:07 -0400

    Added DOTween and Two Menu Open Tween
    We now have an item trigger that when you're inside it, you can get the information about the text by zooming in. It closes when you zoom out again.
    
    The first walking tweens are also in place

---
+++ Assets/Script/scr_ItemTweener.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class scr_ItemTweener : MonoBehaviour
+{
+    void Start()
+    {
+        transform.localScale = new Vector2(transform.localScale.x, 0);
+    }
+
+    private void Update()
+    {
+        //this is just to test the tween
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            TweenOpen();
+        }
+        //this is just to test the tween
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            TweenShut();
+        }
+    }
+
+    public void TweenOpen()
+    {
+        transform.DOScaleY(1f, 1f).SetEase(Ease.OutExpo);
+    }
+
+    public void TweenShut()
+    {
+        transform.DOScaleY(0f, 0.2f).SetEase(Ease.OutExpo);
+    }
+}
