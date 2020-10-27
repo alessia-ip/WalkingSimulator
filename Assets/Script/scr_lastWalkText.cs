0939daf9ecb3dfa6fe171a829f9116bc9cedb126
Alessia <alessia@mipsscan.com>
2020-10-27 17:39:52 -0400

    Dialogue All Added - ending capable

---
+++ Assets/Script/scr_lastWalkText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_lastWalkText : MonoBehaviour
+{
+
+    public GameObject blockHouse;
+    public GameObject blockHouse2;
+
+    public GameObject walkingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(blockHouse.activeInHierarchy);
+        if(blockHouse.activeInHierarchy == false && blockHouse2.activeInHierarchy == false)
+        {
+            walkingText.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
