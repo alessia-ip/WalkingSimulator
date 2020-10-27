c4578e6c0e0a35554d06cad64542793acfcc83e9
Alessia <alessia@mipsscan.com>
2020-10-27 15:16:51 -0400

    New Push
    Adding stuff with katie

--- Assets/Script/displayItemUI.cs
+++ Assets/Script/displayItemUI.cs
@@ -10,6 +10,12 @@ public class displayItemUI : MonoBehaviour
     public string specificObj; //if an object needs a specific action to go with it (like pick up) set the string to name of object
     private bool iBool; //bool used for different purposes.
 
+    public Renderer regFilm;
+    public Renderer redFilm;
+
+    public GameObject helpText;
+    public GameObject door;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +23,28 @@ public class displayItemUI : MonoBehaviour
         myText = GameObject.Find("UI").GetComponent<Text> ();
         myText.color = Color.clear;
     }
-
     // Update is called once per frame
     void Update()
     {
+
+        if(globalVarStorage.hasCamera == true && globalVarStorage.hasFilm == false)
+        {
+            helpText.SetActive(true);
+        }
+
         if(specificObj == "bwFilm" && iBool)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+
+                // here is the film thing 
                 globalVarStorage.hasFilm = true;
+                regFilm.enabled = false;
+                redFilm.enabled = false;
+
+                helpText.SetActive(false);
+                door.SetActive(false);
+
             }
         }
     }
@@ -36,8 +55,8 @@ public class displayItemUI : MonoBehaviour
         {
             if (globalVarStorage.hasCamera)
             {
-                myText.text = myString;
-                myText.color = Color.white;
+               // myText.text = myString;
+              //  myText.color = Color.white;
                 iBool = true;
             }
         }
