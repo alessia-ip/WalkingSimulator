5fd663cf64b29043c7e699b6d68701f497f5cbb0
Alessia <alessia@mipsscan.com>
2020-10-28 00:06:57 -0400

    Last Tweaks

---
+++ Assets/Script/scr_backtomenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_backtomenu : MonoBehaviour
+{
+
+    public scr_titleScreenTweener titleScreenTweener;
+    public scr_panelTweener panelTweener;
+    public string newScene;
+
+    public void TweenOpen()
+    {
+        titleScreenTweener.TweenOpen();
+    }
+
+    public void TweenClosed()
+    {
+        titleScreenTweener.TweenShut();
+    }
+
+    void Start()
+    {
+
+        StartCoroutine(sceneStart());
+    }
+
+    public IEnumerator sceneStart()
+    {
+        yield return new WaitForSeconds(100);
+        panelTweener.TweenPanel();
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(newScene);
+    }
+
+}
